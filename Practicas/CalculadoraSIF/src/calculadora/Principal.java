aa2c408fa83dc067a94d6994b458638d0ab23306
Aldo Méndez <produccionesaldo@gmail.com>
2016-08-16 23:50:45 -0500

    Codigo

---
+++ Practicas/CalculadoraSIF/src/calculadora/Principal.java
@@ -0,0 +1,36 @@
+package calculadora;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args){
+        
+        int seleccionEvento,eventLiDif=0,eventLiRank=0,eventLiCom=0;
+        String[] eventos = {"Tokens","Medley Festa","Score Match","Salir"};
+        String[] combo = {"N/C","C","B","A","S"};
+        String[] rank = {"N/R","C","B","A","S"};
+        String[] dificultad = {"Easy","Normal","Hard","Expert"};
+        
+        JOptionPane.showMessageDialog(null,"¡Bienvenido a Calculadora SIF!","CalculadoraSIF",JOptionPane.INFORMATION_MESSAGE);
+        
+        do{
+            seleccionEvento = JOptionPane.showOptionDialog(null, "Escoge el tipo de evento, o elige salir de la app.", "Tipo de evento", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, eventos, "Tokens");
+            JOptionPane.showMessageDialog(null, "Haz escogido "+eventos[seleccionEvento]);
+            
+            if(seleccionEvento == 0){
+                eventLiDif=JOptionPane.showOptionDialog(null,"Dificultad de la cancion de evento:",eventos[0],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,dificultad,"Easy");
+                eventLiRank=JOptionPane.showOptionDialog(null,"Score Rank obtenido:",eventos[0],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,rank,"N/R");
+                eventLiCom=JOptionPane.showOptionDialog(null,"Combo logrado:",eventos[0],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,combo,"N/C");
+                
+            }else if(seleccionEvento == 1){
+                eventLiDif=JOptionPane.showOptionDialog(null,"Dificultad del Medley:",eventos[1],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,dificultad,"Easy");
+                eventLiRank=JOptionPane.showOptionDialog(null,"Score Rank obtenido:",eventos[1],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,rank,"N/R");
+                eventLiCom=JOptionPane.showOptionDialog(null,"Combo logrado:",eventos[0],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,combo,"N/C");
+                
+            }   
+            
+        }while(seleccionEvento != 3);
+        
+    }
+    
+}
---
+++ Practicas/CalculadoraSIF/src/calculadora/Tokens.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+import javax.swing.*;
+
+public class Tokens {
+    
+    public int sacarPuntos(int x,int y,int z){
+        int puntos = 0;
+        int calc[][][]= new int[4][4][4];
+        
+        calc[0][0][0]=52; 
+        calc[0][0][0]=52;
+        calc[0][0][0]=52;
+        calc[0][0][0]=52;
+        calc[0][0][0]=52;
+        
+        
+        
+        
+        return puntos;
+    }
+
+}
